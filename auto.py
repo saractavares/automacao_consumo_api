02aa70b826ec0fceb131ba2ef76d01b28ba6ba86
Sara Centeno <sara27082011@gmail.com>
2022-02-20 20:22:46 -0300

    percentual de consumo extraído/banco conectado

--- auto.py
+++ auto.py
@@ -1,132 +1,91 @@
-from unittest import TestCase
-from itsdangerous import json
-from matplotlib.pyplot import text
+from xml.etree.ElementTree import tostring
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import Select
+from  sqlalchemy import create_engine
 import pandas as pd
+import pyodbc
 import time
-import os
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 login = 'crmdashboard04@crmdashboard04.onmicrosoft.com'
 passw = '4dm1n@CRM04'
 
-#abrir navegador
-options = Options()
-options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-nav = webdriver.Chrome(chrome_options = options, executable_path="C:\\ChromeDriver\\chromedriver.exe")
-nav.get('https://docs.microsoft.com/en-us/rest/api/power-bi/available-features/get-available-features#code-try-0')
+# Conexão com o servidor
+
+def conexao_db():
+
+    #abrir navegador
+    options = Options()
+    options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+    nav = webdriver.Chrome(chrome_options = options, executable_path="C:\\ChromeDriver\\chromedriver.exe")
+    nav.get('https://docs.microsoft.com/en-us/rest/api/power-bi/available-features/get-available-features#code-try-0')
 
 
-time.sleep(3)
-# botão "sign in"
-nav.find_element_by_xpath('//*[@id="action-panel"]/div/div/div/button').click()
-time.sleep(3)
+    time.sleep(3)
+    # botão "sign in"
+    nav.find_element_by_xpath('//*[@id="action-panel"]/div/div/div/button').click()
+    time.sleep(3)
 
-# encontra campo, passa login e avança
-nav.find_element_by_xpath('//*[@id="i0116"]').send_keys(f'{login}')
-nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
-time.sleep(5)
+    # encontra campo, passa login e avança
+    nav.find_element_by_xpath('//*[@id="i0116"]').send_keys(f'{login}')
+    nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
+    time.sleep(5)
 
-# encontra campo, passa senha e avança
-nav.find_element_by_xpath('//*[@id="i0118"]').send_keys(f'{passw}')
-nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
-time.sleep(5)
+    # encontra campo, passa senha e avança
+    nav.find_element_by_xpath('//*[@id="i0118"]').send_keys(f'{passw}')
+    nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
+    time.sleep(5)
 
-# escolhe não continuar conectado
-nav.find_element_by_xpath('//*[@id="idBtn_Back"]').click()
-time.sleep(5)
+    # escolhe não continuar conectado
+    nav.find_element_by_xpath('//*[@id="idBtn_Back"]').click()
+    time.sleep(5)
 
-# botão "try it"
-nav.find_element_by_xpath('//*[@id="code-try-0"]/button[2]').click()
-time.sleep(3)
+    # botão "try it"
+    nav.find_element_by_xpath('//*[@id="code-try-0"]/button[2]').click()
+    time.sleep(3)
 
-# clica no botão "Run"
-nav.find_element_by_xpath('//*[@id="action-panel"]/div/form/div[2]/div[5]/button').click()
-time.sleep(5)
-# print(nav.find_element_by_xpath('//*[@id="action-panel"]/div/form/div[3]/div[1]/div/button').click())
+    # clica no botão "Run"
+    nav.find_element_by_xpath('//*[@id="action-panel"]/div/form/div[2]/div[5]/button').click()
+    time.sleep(5)
 
-# resultado = nav.find_element_by_xpath('//*[@id="action-panel"]/div/form/div[3]/h2/span').get_attribute(text)
+    # armazenando a % do consumo atual
+    usage= nav.find_element_by_xpath('/html/body/div[3]/div/form/div[3]/div[2]/pre/span/span[18]').text
+    
 
-# print(resultado)
+    uso = str(usage)
+    print(uso)
 
+    time.sleep(3)
 
-# div = nav.find_elements_by_xpath('//*[@id="action-panel"]/div/form/div[3]/div[2]')
+    # Server = 'tcp:f4f8ugzf66.database.windows.net'
+    # Database = 'LEADSCORING'
+    # Driver = 'ODBC Driver 17 for SQL Server'
+    User = 'DataScience'
+    Passw = "brasil@1'"
+    # Database_con = f"mssql+pyodbc://{User}:{Passw}@tcp:f4f8ugzf66.database.windows.net/LEADSCORING?driver=ODBC Driver 17 for SQL Server"
 
-# div = open('div_resultado.txt','w')
-# div.write(div)
-# div.close()
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:f4f8ugzf66.database.windows.net;DATABASE=LEADSCORING;UID=DataScience;PWD=brasil@1')
 
-# resultado = nav.find_element_by_xpath('//*[@id="action-panel"]/div/form/div[3]/div[2]/pre/span/span[18]').get_attribute(text)
+    # engine = create_engine(f"mssql+pyodbc://{User}:{Passw}@tcp:f4f8ugzf66.database.windows.net/LEADSCORING?driver=ODBC+Driver+17+for+SQL+Server")
+    # conn = engine.connect()
 
-# print(div)
+    #  Conexão com o servidor
+    # conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; \
+    #                     SERVER=tcp:f4f8ugzf66.database.windows.net; \
+    #                     DATABASE= LEADSCORING; \
+    #                     UID=DataScience; \
+    #                     PWD=brasil@1')
+    print(uso)
+    # query =                         
+    # query_select = "SELECT * FROM [dbo].[API_CONSUMO_RECURSO]"                    
+    # print(type(query))
+    base = pd.read_sql_query(f"INSERT INTO API_CONSUMO_RECURSO (perc_consumo) VALUES ('{uso}')", conn)
+    # conn.commit()
+    base
 
-# nav.switch_to.window(driver.window_handles[1])
-# nav.get('https://www.google.com/')
-# nav.find_element_by_xpath('')
-
-
-# resultado = nav.find_element_by_xpath('//*[@id="action-panel"]/div/form/div[3]/div[2]/pre/span/span[18]').send_keys('ctrl','v')
-
-time.sleep(3)
+conexao_db()
 
 print("Terminado Aqui")
-
-# dolar = nav.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-# print(dolar)
-
-# nav.get('https://www.google.com/')
-
-# nav.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação euro agora')
-# nav.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]').send_keys(Keys.ENTER)
-
-# euro = nav.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-# print(euro)
-
-# nav.get('https://www.melhorcambio.com/ouro-hoje#:~:text=O%20valor%20do%20grama%20do,em%20R%24%20280%2C60.')
-
-# ouro = nav.find_element_by_xpath('//*[@id="comercial"]').get_attribute('value')
-# ouro = ouro.replace(',','.')
-# print(ouro)
-# nav.quit()
-
-
-# # ### Agora vamos atualiza a nossa base de preços com as novas cotações
-
-# # - Importando a base de dados
-
-# # In[69]:
-
-
-# import pandas as pd
-
-# tb = pd.read_excel(r'C:\repoteste_api\Produtos.xlsx')
-# tb.head()
-
-
-# # - Atualizando os preços e o cálculo do Preço Final
-
-# # In[70]:
-
-
-# tb.loc[tb["Moeda"] == "Dólar", "Cotação"] = float(dolar)
-# tb.loc[tb["Moeda"] == "Euro", "Cotação"] = float(euro)
-# tb.loc[tb["Moeda"] == "Ouro", "Cotação"] = float(ouro)
-
-# tb["Preço Base Reais"] = tb["Preço Base Original"] * tb["Cotação"]
-
-# tb["Preço Final"] = tb["Preço Base Reais"] * tb["Margem"]
-
-# tb.head()
-
-
-# # ### Agora vamos exportar a nova base de preços atualizada
-
-# # In[71]:
-
-
-# tb.to_excel("Produtos Novo1.xlsx", index=False)
