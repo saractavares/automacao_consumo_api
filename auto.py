d1bce46aaa2d5ab5991323d4f0a69dc2af0abad7
Sara Centeno <sara27082011@gmail.com>
2022-02-22 15:24:27 -0300

    email sender feito, regra >65

--- auto.py
+++ auto.py
@@ -1,14 +1,17 @@
-from datetime import date
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from datetime import date
 import pandas as pd
 import pyodbc
 import time
 
-
 # Conectando ao banco e conferindo Conta Ativa
-con = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:f4f8ugzf66.database.windows.net;DATABASE=LEADSCORING;UID=DataScience;PWD=brasil@1;Trusted_Connection=no')
-
+try:
+    con = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:f4f8ugzf66.database.windows.net;DATABASE=CONSUMO_DASH;UID=DataScience;PWD=brasil@1;Trusted_Connection=no')
+    print('Conexão com banco estabelecida')
+except:
+    print('Conexão com Banco Perdida')
 query = """select nome, usuario, senha from CONTA_API_CONSUMO
            where data_final is NULL;"""
 base = pd.read_sql_query(query, con)
@@ -21,77 +24,97 @@ for i in range(len(base)):
 login = usuario
 passw = senha
 
-# Busca e Raspagem
-def automacao():
+def extract():
 
-    #abrir navegador
+    # abrir navegador
     options = Options()
     options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-    nav = webdriver.Chrome(chrome_options = options, executable_path="C:\\ChromeDriver\\chromedriver.exe")
-    try:
-        nav.get('https://docs.microsoft.com/en-us/rest/api/power-bi/available-features/get-available-features#code-try-0')
+    nav = webdriver.Chrome(
+        chrome_options=options, executable_path="C:\\ChromeDriver\\chromedriver.exe")
 
-        time.sleep(3)
+    nav.get(
+        'https://docs.microsoft.com/en-us/rest/api/power-bi/available-features/get-available-features#code-try-0')
 
-        # botão "sign in"
-        nav.find_element_by_xpath('//*[@id="action-panel"]/div/div/div/button').click()
+    time.sleep(3)
 
-        time.sleep(3)
+    # botão "sign in"
+    nav.find_element_by_xpath(
+        '//*[@id="action-panel"]/div/div/div/button').click()
 
-        # encontra campo, passa login e avança
-        nav.find_element_by_xpath('//*[@id="i0116"]').send_keys(f'{login}')
-        nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
+    time.sleep(3)
 
-        time.sleep(3)
+    # encontra campo, passa login e avança
+    nav.find_element_by_xpath('//*[@id="i0116"]').send_keys(f'{login}')
+    nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
 
-        # encontra campo, passa senha e avança
-        nav.find_element_by_xpath('//*[@id="i0118"]').send_keys(f'{passw}')
-        nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
+    time.sleep(3)
 
-        time.sleep(3)
+    # encontra campo, passa senha e avança
+    nav.find_element_by_xpath('//*[@id="i0118"]').send_keys(f'{passw}')
+    nav.find_element_by_xpath('//*[@id="idSIButton9"]').click()
 
-        # escolhe não continuar conectado
-        nav.find_element_by_xpath('//*[@id="idBtn_Back"]').click()
+    time.sleep(3)
 
-        time.sleep(3)
+    # escolhe não continuar conectado
+    nav.find_element_by_xpath('//*[@id="idBtn_Back"]').click()
 
-        # botão "try it"
-        nav.find_element_by_xpath('//*[@id="code-try-0"]/button[2]').click()
+    time.sleep(3)
 
-        time.sleep(3)
+    # botão "try it"
+    nav.find_element_by_xpath(
+        '//*[@id="code-try-0"]/button[2]').click()
 
-        # clica no botão "Run"
-        nav.find_element_by_xpath('//*[@id="action-panel"]/div/form/div[2]/div[5]/button').click()
+    time.sleep(3)
 
-        time.sleep(3)
+    # clica no botão "Run"
+    nav.find_element_by_xpath(
+        '//*[@id="action-panel"]/div/form/div[2]/div[5]/button').click()
 
-        # armazenando a % do consumo atual
-        uso = nav.find_element_by_xpath('/html/body/div[3]/div/form/div[3]/div[2]/pre/span/span[18]').text
+    time.sleep(3)
 
-        time.sleep(3)  
+    # armazenando a % do consumo atual
+    uso = nav.find_element_by_xpath(
+        '/html/body/div[3]/div/form/div[3]/div[2]/pre/span/span[18]').text
 
-        # parâmetros para inserir dados na tabela de consumo
-        nome = conta
-        df_uso = uso
-        data_atual = date.today()
+    time.sleep(3)
 
-        uso = {'nome':[nome],'uso':[df_uso],'data_atual':[data_atual]}
-        tb_consumo = pd.DataFrame(uso)
+    # parâmetros para inserir dados na tabela de consumo
+    nome = conta
+    df_uso = uso
+    data_atual = date.today()
 
-        # Cursor para o banco
-        cursor = con.cursor()                    
-        insert_str = """INSERT INTO dbo.CONSUMO_API_RECURSO(nome,uso,data_atual)
-            values(?,?,?)"""
-        cursor.fast_executemany = True
-        cursor.executemany(insert_str,  tb_consumo.values.tolist())                  
-        con.commit()
-        con.close()
-    except:
-        nav.close()
-        print('Houve Alguma Falha Inexperada. Vamos Recomeçar o Processo. Você Pode Cancelar o Processo a Qualquer Momento.')
-        automacao()
+    uso = {'nome': [nome], 'uso': [df_uso], 'data_atual': [data_atual]}
+    tb_consumo = pd.DataFrame(uso)
+
+    # Cursor para o banco
+    cursor = con.cursor()
+    insert_str = """INSERT INTO dbo.CONSUMO_API_RECURSO(nome,uso,data_atual)
+        values(?,?,?)"""
+    cursor.fast_executemany = True
+    cursor.executemany(insert_str,  tb_consumo.values.tolist())
+    con.commit()
+    # con.close()
+    print(
+        "******** RASPAGEM DO CONSUMO E ATUALIZAÇÃO DO BANCO DE DADOS CONCLUÍDA ********")
+    print()
+    print('iniciando envio de email')
+
+    nav.close()
     
+extract()
 
-automacao()
+# Conferindo se o uso chegou em 65% e se sim, mandar o email de aviso
+query = """SELECT [uso] FROM [dbo].[CONSUMO_API_RECURSO] WHERE data_atual = CONVERT (DATE, SYSDATETIME())"""
+bd = pd.read_sql_query(query, con)
+for i in range(len(bd)):
+        uso_atual = str(bd.uso[i].strip())
+        uso_atual = int(uso_atual)
 
-print("******** RASPAGEM DO CONSUMO E ATUALIZAÇÃO DO BANCO DE DADOS CONCLUÍDA ********")
+if uso_atual >= 65:
+    import sender
+    sender    
+else:
+    print(f'\nO Uso do Recurso Está em {uso_atual}')  
+
+print()
+print('PROCESSO COMPLETO CONCLUÍDO')
---
+++ sender.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask_mail import Mail, Message
+import pandas as pd
+import time
+import pyodbc
+
+
+print('Init DB')
+con = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:f4f8ugzf66.database.windows.net;DATABASE=CONSUMO_DASH;UID=DataScience;PWD=brasil@1;Trusted_Connection=no')
+
+# cursor = con.cursor()
+query = """SELECT [nome],[uso],[data_atual] FROM [dbo].[CONSUMO_API_RECURSO] WHERE data_atual = CONVERT (DATE, SYSDATETIME())"""
+bd = pd.read_sql_query(query, con)
+
+
+for i in range(len(bd)):
+        conta = str(bd.nome[i].strip())
+        uso = str(bd.uso[i].strip())
+        data = str(bd.data_atual[i])
+
+        print(conta, uso, data)
+
+print('Peguei as credenciais')
+
+
+app = Flask(__name__)
+with app.app_context():
+
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 587
+    app.config["MAIL_USE_TLS"] = True
+    app.config["MAIL_USE_SSL"] = False
+    app.config["MAIL_USERNAME"] = "apicrmconsumo@gmail.com"
+    app.config["MAIL_PASSWORD"] = "ap1con5umo@crm"
+    app.config["MAIL_DEFAULT_SENDER"] = "apicrmconsumo@gmail.com"
+    app.config["MAIL_MAX_EMAILS"] = 4
+    app.config["MAIL_ASCII_ATTACHMENTS"] = True
+    app.config["MAIL_TO"] = ["luiz.felipe@crmeducacional.com"]
+
+    mail = Mail(app)
+
+    msg = Message(" Report Diário do Consumo do Recurso",
+                recipients=app.config["MAIL_TO"])
+
+    msg.body = f'Olá, Luiz Felipe! \nO recurso de Dashboards da {conta} foi extraído em {data} e está em {uso}% !'
+
+    mail.send(msg)
+
+    time.sleep(3)
+
+    print('Email Enviado')
+
+    # send_email()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
